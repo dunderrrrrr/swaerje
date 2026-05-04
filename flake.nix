ea7a57775cecc74fb4f874956c975eb74cb00903
dunderrrrrr <emil.bjorkroth@gmail.com>
2026-05-04 21:26:38 +0200

    drop fucking devenv shit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        pkgs,
+        lib,
+        ...
+      }: let
+        python = pkgs.python312;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.uv
+            pkgs.ruff
+            python
+          ];
+          shellHook = ''
+            uv venv
+            source .venv/bin/activate
+            uv pip sync requirements.txt
+            pre-commit install --overwrite
+            set -a
+            source .env 2> /dev/null
+          '';
+        };
+      };
+    };
+}
